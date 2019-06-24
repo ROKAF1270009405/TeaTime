26a73f9fc5697c3748e5fd4b387eb45d0074b5ef
park <hong@naver.com>
2019-06-24 15:57:16 +0900

    dto 수정입니다.

--- src/com/teatime/dto/CustomerServiceDTO.java
+++ src/com/teatime/dto/CustomerServiceDTO.java
@@ -6,40 +6,59 @@ public class CustomerServiceDTO {
 	private String id;        // 아이디
 	private String title;     // 제목
 	private String content;   // 내용
-	
-	public CustomerServiceDTO() {}
-	public CustomerServiceDTO(int qnano, String id, String title, String content) {
+	private String csreply;   // 답변
+
+	public CustomerServiceDTO(int qnano, String id, String title, String content, String csreply) {
+		super();
 		this.qnano = qnano;
 		this.id = id;
 		this.title = title;
 		this.content = content;
+		this.csreply = csreply;
 	}
-	
+
+	public CustomerServiceDTO() {
+
+	}
+
 	public int getQnano() {
 		return qnano;
 	}
+
 	public void setQnano(int qnano) {
 		this.qnano = qnano;
 	}
+
 	public String getId() {
 		return id;
 	}
+
 	public void setId(String id) {
 		this.id = id;
 	}
+
 	public String getTitle() {
 		return title;
 	}
+
 	public void setTitle(String title) {
 		this.title = title;
 	}
+
 	public String getContent() {
 		return content;
 	}
+
 	public void setContent(String content) {
 		this.content = content;
 	}
-	
-	
+
+	public String getCsreply() {
+		return csreply;
+	}
+
+	public void setCsreply(String csreply) {
+		this.csreply = csreply;
+	}
 
 } // end customerServiceDTO class
