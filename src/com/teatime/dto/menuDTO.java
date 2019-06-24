075bc4b83d845ff9cd45eb4d2a9f07f06713e69b
park <hong@naver.com>
2019-06-24 13:20:31 +0900

    DTO 수정입니다.

--- src/com/teatime/dto/menuDTO.java
+++ src/com/teatime/dto/menuDTO.java
@@ -2,35 +2,43 @@ package com.teatime.dto;
 
 public class menuDTO {
 
-	private int menuno;
-	private String name;
-	private int price;
-	private String foodkind;
-	
+	private int menuno;        // 메뉴 번호
+	private String name;       // 이름
+	private int price;         // 가격
+	private String foodkind;   // 음식 종류
+
 	public int getMenuno() {
 		return menuno;
 	}
+
 	public void setMenuno(int menuno) {
 		this.menuno = menuno;
 	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public int getPrice() {
 		return price;
 	}
+
 	public void setPrice(int price) {
 		this.price = price;
 	}
+
 	public String getFoodkind() {
 		return foodkind;
 	}
+
 	public void setFoodkind(String foodkind) {
 		this.foodkind = foodkind;
 	}
+
 	public menuDTO(int menuno, String name, int price, String foodkind) {
 		super();
 		this.menuno = menuno;
@@ -38,9 +46,9 @@ public class menuDTO {
 		this.price = price;
 		this.foodkind = foodkind;
 	}
+
 	public menuDTO() {
 		super();
 	}
-	
-	
+
 }
--- src/com/teatime/dto/reviewDTO.java
+++ src/com/teatime/dto/reviewDTO.java
@@ -2,64 +2,79 @@ package com.teatime.dto;
 
 public class reviewDTO {
 
-	private int reviewno;
-	private String title;
-	private String content;
-	private String date;
-	private String photo;
-	private float gpa;
-	private int shopno;
-	private String id;
-	
+	private int reviewno;     // 리뷰 번호
+	private String title;     // 제목
+	private String content;   // 내용
+	private String date;      // 등록일
+	private String photo;     // 사진
+	private float gpa;        // 평점
+	private int shopno;       // 매장 번호
+	private String id;        // 아이디
+
 	public int getReviewno() {
 		return reviewno;
 	}
+
 	public void setReviewno(int reviewno) {
 		this.reviewno = reviewno;
 	}
+
 	public String getTitle() {
 		return title;
 	}
+
 	public void setTitle(String title) {
 		this.title = title;
 	}
+
 	public String getContent() {
 		return content;
 	}
+
 	public void setContent(String content) {
 		this.content = content;
 	}
+
 	public String getDate() {
 		return date;
 	}
+
 	public void setDate(String date) {
 		this.date = date;
 	}
+
 	public String getPhoto() {
 		return photo;
 	}
+
 	public void setPhoto(String photo) {
 		this.photo = photo;
 	}
+
 	public float getGpa() {
 		return gpa;
 	}
+
 	public void setGpa(float gpa) {
 		this.gpa = gpa;
 	}
+
 	public int getShopno() {
 		return shopno;
 	}
+
 	public void setShopno(int shopno) {
 		this.shopno = shopno;
 	}
+
 	public String getId() {
 		return id;
 	}
+
 	public void setId(String id) {
 		this.id = id;
 	}
-	
+
 	public reviewDTO(int reviewno, String title, String content, String date, String photo, float gpa, int shopno,
 			String id) {
 		super();
@@ -72,10 +87,9 @@ public class reviewDTO {
 		this.shopno = shopno;
 		this.id = id;
 	}
+
 	public reviewDTO() {
 		super();
 	}
-	
-	
-	
+
 }
