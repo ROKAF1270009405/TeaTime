06da9f05368e1652e8257268e8185206711e8e6d
yong <yong@naver.com>
2019-06-26 20:00:46 +0900

    cs reply action 작성중

---
+++ src/com/teatime/cs/action/CSReplyAction.java
@@ -0,0 +1,21 @@
+package com.teatime.cs.action;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.teatime.comm.Action;
+import com.teatime.comm.ActionForward;
+
+public class CSReplyAction implements Action {
+
+	@Override
+	public ActionForward execute(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
